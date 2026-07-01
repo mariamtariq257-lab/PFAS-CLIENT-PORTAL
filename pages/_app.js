1058ae01743a7e6a171c00e6e4172fc1b7f280bb
mariamtariq257-lab <mariamtariq257@gmail.com>
2026-07-01 15:18:33 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 // pages/_app.js
 import { SessionProvider } from "next-auth/react";
-import "../styles/globals.css";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
